620799f9c540e05851ffe0cc3519a2cae027449a
권대선 <dsun@hanmail.net>
2021-02-24 16:39:28 +0900

    Initial commit

---
+++ src/View/DictionaryWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using Atomus.Control;
+using Atomus.Control.Dictionary;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Atomus.Windows.Controls.Dictionary
+{
+    /// <summary>
+    /// DictionaryWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class DictionaryWindow : Window, IDictionary, IAction
+    {
+        private AtomusControlEventHandler beforeActionEventHandler;
+        private AtomusControlEventHandler afterActionEventHandler;
+
+        string IDictionary.Code { get; set; }
+        string IDictionary.SearchText { get; set; }
+        int IDictionary.SearchIndex { get; set; }
+        string IBeforeEventArgs.Where { get; set; }
+        bool IBeforeEventArgs.SearchAll { get; set; }
+        bool IBeforeEventArgs.StartsWith { get; set; }
+        DataRow IAfterEventArgs.DataRow { get; set; }
+        DataTable IAfterEventArgs.DataTable { get; set; }
+        BeforeAction IDictionary.BeforeAction { get; set; }
+        AfterAction IDictionary.AfterAction { get; set; }
+
+        #region Init
+        public DictionaryWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ViewModel.DictionaryWindowViewModel(this);
+        }
+        #endregion
+
+        #region IO
+        object IAction.ControlAction(ICore sender, AtomusControlArgs e)
+        {
+            try
+            {
+                this.beforeActionEventHandler?.Invoke(this, e);
+
+                switch (e.Action)
+                {
+                    case "Selected":
+                        this.DictionaryWindow_Deactivated(this, null);
+                        return true;
+
+                    case "ADD_COMBO_ITEM":
+                        if(e.Value == null)
+                        { 
+                            (this.DataContext as ViewModel.DictionaryWindowViewModel).SearchSyncCommand.Execute(null);
+                            if ((this as IDictionary).AfterAction != null)
+                            {
+                                (this as IDictionary).DataRow = (this as IDictionary).DataTable.Rows[0];
+                                (this as IDictionary).AfterAction.Invoke(null, (this as IDictionary));
+                            }
+                        }
+                        else if(e.Value is List<object>)
+                        {
+                            // [0]: Code                (string)
+                            // [1]: SearchIndex         (int)
+                            // [2]: SearchText          (string)
+                            // [3]: Target              (ComboBox)
+                            // [4]: DisplayMemberPath   (string)
+                            // [5]: SelectedValuePath   (string)
+
+                            List<object> data = e.Value as List<object>;
+                            (this as IDictionary).Code = data[0].ToString();
+                            (this as IDictionary).SearchIndex = data[1].ToString().ToInt();
+                            (this as IDictionary).SearchText = data[2].ToString();
+                            ComboBox combo = data[3] as ComboBox;
+                            combo.DisplayMemberPath = data[4].ToString();
+                            combo.SelectedValuePath = data[5].ToString();
+
+                            (this.DataContext as ViewModel.DictionaryWindowViewModel).SearchSyncCommand.Execute(null);                            
+                            combo.ItemsSource = (this as IDictionary).DataTable.DefaultView;                            
+                        }
+                        return true;
+
+                    default:
+                        throw new AtomusException("'{0}'은 처리할 수 없는 Action 입니다.".Translate(e.Action));
+                }
+            }
+            finally
+            {
+                this.afterActionEventHandler?.Invoke(this, e);
+            }
+        }
+        #endregion
+
+        #region Event
+        event AtomusControlEventHandler IAction.BeforeActionEventHandler
+        {
+            add
+            {
+                this.beforeActionEventHandler += value;
+            }
+            remove
+            {
+                this.beforeActionEventHandler -= value;
+            }
+        }
+        event AtomusControlEventHandler IAction.AfterActionEventHandler
+        {
+            add
+            {
+                this.afterActionEventHandler += value;
+            }
+            remove
+            {
+                this.afterActionEventHandler -= value;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            (this.DataContext as ViewModel.DictionaryWindowViewModel).SearchCommand.Execute(null);
+            this.SearchText.Focus();
+        }
+
+        public void AdjustPosition()
+        {
+            try
+            {
+                int cnt = (this as IDictionary).DataTable.Rows.Count;
+                int height = (cnt * 25) + 90 > 590 ? 610 : (cnt * 25) + 110;
+
+                if (this.Top + height > Application.Current.MainWindow.ActualHeight)
+                {
+                    this.Top = this.Top - height;
+                    if (this.Top < 0)
+                        this.Top = 0;
+                }
+
+                if (this.Left + (this.DataContext as ViewModel.DictionaryWindowViewModel).WidthCalculated > Application.Current.MainWindow.ActualWidth)
+                {
+                    this.Left = Application.Current.MainWindow.ActualWidth - (this.DataContext as ViewModel.DictionaryWindowViewModel).WidthCalculated - 20;
+                    if (this.Left < 0)
+                        this.Left = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Diagnostics.DiagnosticsTool.MyTrace(ex);
+            }
+        }
+
+        private void DictionaryWindow_Deactivated(object sender, EventArgs e)
+        {
+            if (this.IsVisible)
+                this.Close();
+        }
+        #endregion
+
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            DataTable dataTable;
+
+            if (sender is DataGrid && (sender as DataGrid).ItemsSource != null && (sender as DataGrid).ItemsSource is DataView)
+            {
+                dataTable = ((sender as DataGrid).ItemsSource as DataView).Table;
+
+                foreach (DataColumn dataColumn in dataTable.Columns)
+                {
+                    if (dataColumn.Caption.Contains('^') && dataColumn.ColumnName == (e.Column.Header as string))
+                        e.Column.Width = new DataGridLength(dataColumn.Caption.Split('^')[1].ToDouble());
+
+                    // 헤더에 부적합 문자
+                    if (dataColumn.ColumnName.Contains('/'))
+                        dataColumn.ColumnName = dataColumn.ColumnName.Replace('/', '_');
+                }
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
---
+++ src/ViewModel/DictionaryWindowViewModel.cs
@@ -0,0 +1,369 @@
+﻿using Atomus.Control;
+using Atomus.Diagnostics;
+using Atomus.Service;
+using Atomus.Windows.Controls.Dictionary.Controllers;
+using Atomus.Windows.Controls.Dictionary.Models;
+using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Atomus.Windows.Controls.Dictionary.ViewModel
+{
+    public class DictionaryWindowViewModel : Atomus.MVVM.ViewModel
+    {
+        #region Declare
+        private double _WIdthCalculated;
+
+        public double WidthCalculated
+        {
+            get { return _WIdthCalculated; }
+            set
+            {
+                if (this._WIdthCalculated != value)
+                {
+                    this._WIdthCalculated = value; this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _TitleText;
+
+        public string TitleText
+        {
+            get { return _TitleText; }
+            set
+            {
+                if (this._TitleText != value)
+                {
+                    this._TitleText = value; this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private bool _IsSearchAll;
+
+        public bool IsSearchAll
+        {
+            get { return _IsSearchAll; }
+            set
+            {
+                if (this._IsSearchAll != value)
+                {
+                    this._IsSearchAll = value; this.NotifyPropertyChanged();
+
+                    // 체크시 전체 검색
+                    if (IsSearchAll)
+                    {
+                        SearchText = "";
+                        SearchCommand.Execute(null);
+                    }
+                }
+            }
+        }
+
+        private bool isEnabledControl;
+
+        public bool IsEnabledControl
+        {
+            get
+            {
+                return this.isEnabledControl;
+            }
+            set
+            {
+                if (this.isEnabledControl != value)
+                {
+                    this.isEnabledControl = value; this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private DataView _CurrentDataView;
+
+        public DataView CurrentDataView
+        {
+            get { return _CurrentDataView; }
+            set
+            {
+                if (this._CurrentDataView != value)
+                {
+                    this._CurrentDataView = value; this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private DataRowView _SelectedDataRowView;
+
+        public DataRowView SelectedDataRowView
+        {
+            get { return _SelectedDataRowView; }
+            set
+            {
+                if (this._SelectedDataRowView != value)
+                {
+                    this._SelectedDataRowView = value; this.NotifyPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region Property
+        public ICore Core { get; set; }
+        public ICore ParentCore { get; set; }
+
+        public string SearchText
+        {
+            get
+            {
+                if (this.Core != null)
+                    return (this.Core as IDictionary).SearchText;
+                else
+                    return null;
+            }
+            set
+            {
+                if ((this.Core as IDictionary).SearchText != value)
+                {
+                    (this.Core as IDictionary).SearchText = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand SearchCommand { get; set; }
+        public ICommand LeftDoubleClickCommand { get; set; }
+        public ICommand SearchSyncCommand { get; set; }
+        #endregion
+
+        #region INIT
+        public DictionaryWindowViewModel()
+        {
+            this.isEnabledControl = true;
+
+            this.SearchCommand = new MVVM.DelegateCommand(async () => { await this.SearchProcess(); }
+                                                            , () => { return this.isEnabledControl; });
+
+            this.LeftDoubleClickCommand = new MVVM.DelegateCommand(() => { this.LeftDoubleClickProcess(); }
+                                                            , () => { return this.isEnabledControl; });
+
+            this.SearchSyncCommand = new MVVM.DelegateCommand(() => { this.SearchSyncProcess(); }
+                                                            , () => { return this.isEnabledControl; });
+
+        }
+        public DictionaryWindowViewModel(ICore core) : this()
+        {
+            this.Core = core;
+        }
+        #endregion
+
+        #region IO
+        private async Task SearchProcess()
+        {
+            //System.Diagnostics.Trace.WriteLine("LEFT: " +(this.Core as Window).Left + " TOP: " + (this.Core as Window).Top);
+
+            IDictionary dictionary;
+            IResponse result;
+            string[] tmps;
+            double tmpDouble;
+
+            dictionary = null;
+
+            try
+            {
+                this.IsEnabledControl = false;
+                (this.SearchCommand as Atomus.MVVM.DelegateCommand).RaiseCanExecuteChanged();
+                (this.Core as DictionaryWindow).Cursor = Cursors.Wait;
+
+                dictionary = (this.Core as IDictionary);
+
+                if (dictionary.BeforeAction != null && !dictionary.BeforeAction.Invoke(null, dictionary))
+                    return;
+
+                result = await this.Core.SearchAsync(new WindowsControlSearchModel()
+                {
+                    CODE = dictionary.Code,
+                    SEARCH_TEXT = dictionary.SearchText,
+                    SEARCH_INDEX = dictionary.SearchIndex + 1,
+                    COND_ETC = dictionary.Where,
+                    SEARCH_ALL = dictionary.SearchAll ? "Y" : "N",
+                    STARTS_WITH = dictionary.StartsWith ? "Y" : "N"
+                }, this.ParentCore);
+
+                if (result.Status == Status.OK)
+                {
+                    if (result.DataSet.Tables.Count >= 1)
+                    {
+                        tmpDouble = 30;
+                        foreach (DataColumn _DataColumn in result.DataSet.Tables[0].Columns)
+                        {
+                            if (_DataColumn.Caption.Contains('^'))
+                            {
+                                tmps = _DataColumn.Caption.Split('^');
+                                tmpDouble += tmps[1].ToDouble();
+                            }
+                        }
+
+                        if (tmpDouble < 70)
+                            tmpDouble = 70;
+
+                        this.CurrentDataView = result.DataSet.Tables[0].DefaultView;
+
+                        dictionary.DataTable = this.CurrentDataView.Table;
+
+                        this.SetForm(tmpDouble);
+
+                        if ((this.Core as Window).Tag == null)
+                        {
+                            this.TitleText = dictionary.Code.ToString() +
+                            " (" + CurrentDataView.Table.Rows.Count + "/" + CurrentDataView.Table.Rows.Count + ")";
+                        }
+                        else
+                        {
+                            this.TitleText = (this.Core as Window).Tag.ToString() +
+                            " (" + CurrentDataView.Table.Rows.Count + "/" + CurrentDataView.Table.Rows.Count + ")";
+                        }
+
+                        (this.Core as DictionaryWindow).AdjustPosition();
+                    }
+
+                    return;
+                }
+                else
+                    throw new AtomusException(result.Message);
+
+            }
+            catch (Exception ex)
+            {
+                this.WindowsMessageBoxShow(Application.Current.Windows[0], ex);
+            }
+            finally
+            {
+                this.IsEnabledControl = true;
+                (this.SearchCommand as Atomus.MVVM.DelegateCommand).RaiseCanExecuteChanged();
+                (this.Core as DictionaryWindow).Cursor = Cursors.Arrow;
+            }
+        }
+
+        private void LeftDoubleClickProcess()
+        {
+            IDictionary dictionary;
+
+            try
+            {
+                (this.LeftDoubleClickCommand as Atomus.MVVM.DelegateCommand).RaiseCanExecuteChanged();
+
+                if (this.SelectedDataRowView != null)
+                {
+                    dictionary = (this.Core as IDictionary);
+
+                    dictionary.DataRow = this.SelectedDataRowView.Row;
+
+                    if (dictionary.AfterAction != null && dictionary.AfterAction.Invoke(null, dictionary))
+                    {
+                    }
+                    else
+                    {
+                        dictionary.DataRow = null;
+                    }
+
+                    (this.Core as IAction).ControlAction(this.Core, "Selected", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.WindowsMessageBoxShow(Application.Current.Windows[0], ex);
+            }
+            finally
+            {
+                this.IsEnabledControl = true;
+                (this.LeftDoubleClickCommand as Atomus.MVVM.DelegateCommand).RaiseCanExecuteChanged();
+            }
+        }
+
+        private void SearchSyncProcess()
+        {
+            IDictionary dictionary;
+            IResponse result;
+            string[] tmps;
+            double tmpDouble;
+            dictionary = null;
+
+            try
+            {
+                this.IsEnabledControl = false;
+                (this.SearchCommand as Atomus.MVVM.DelegateCommand).RaiseCanExecuteChanged();
+
+                dictionary = (this.Core as IDictionary);
+
+                if (dictionary.BeforeAction != null && !dictionary.BeforeAction.Invoke(null, dictionary))
+                    return;
+
+                result = this.Core.Search(new WindowsControlSearchModel()
+                {
+                    CODE = dictionary.Code,
+                    SEARCH_TEXT = dictionary.SearchText,
+                    SEARCH_INDEX = dictionary.SearchIndex + 1,
+                    COND_ETC = dictionary.Where,
+                    SEARCH_ALL = dictionary.SearchAll ? "Y" : "N",
+                    STARTS_WITH = dictionary.StartsWith ? "Y" : "N"
+                }, this.ParentCore);
+
+                if (result.Status == Status.OK)
+                {
+                    if (result.DataSet.Tables.Count >= 1)
+                    {
+                        tmpDouble = 30;
+                        foreach (DataColumn _DataColumn in result.DataSet.Tables[0].Columns)
+                        {
+                            if (_DataColumn.Caption.Contains('^'))
+                            {
+                                tmps = _DataColumn.Caption.Split('^');
+                                tmpDouble += tmps[1].ToDouble();
+                            }
+                        }
+
+                        this.CurrentDataView = result.DataSet.Tables[0].DefaultView;
+
+                        dictionary.DataTable = this.CurrentDataView.Table;
+                    }
+
+                    return;
+                }
+                else
+                    throw new AtomusException(result.Message);
+
+            }
+            catch (Exception ex)
+            {
+                this.WindowsMessageBoxShow(Application.Current.Windows[0], ex);
+            }
+            finally
+            {
+                this.IsEnabledControl = true;
+                (this.SearchCommand as Atomus.MVVM.DelegateCommand).RaiseCanExecuteChanged();
+            }
+        }
+        #endregion
+
+        #region ETC
+        private void SetForm(double tmpWidth)
+        {
+            IDictionary dictionary;
+
+            try
+            {
+                dictionary = (this as IDictionary);
+
+                WidthCalculated = tmpWidth;
+            }
+            catch (Exception exception)
+            {
+                DiagnosticsTool.MyTrace(exception);
+            }
+        }
+        #endregion
+    }
+}
